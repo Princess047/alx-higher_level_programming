1a8d95aa84cee00c7458cb1881d91dfb8aa46100
Princess047 <princessemmanuel497@gmail.com>
2023-08-13 03:21:26 -0700

    task 3 updated, linked lists

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = [1, 2, 3]
+print_reversed_list_integer(my_list)
